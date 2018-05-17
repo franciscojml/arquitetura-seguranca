4623886d61cee67cb605a4a38723244d6bc1bd9c
Francisco Lima <franciscojml@gmail.com>
2018-05-17 09:50:15 -0300

    Projeto Inicial

---
+++ src/main/java/br/embrapa/arquitetura/seguranca/ArquiteturaSegurancaApplication.java
@@ -0,0 +1,12 @@
+package br.embrapa.arquitetura.seguranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaSegurancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaSegurancaApplication.class, args);
+	}
+}
---
+++ src/test/java/br/embrapa/arquitetura/seguranca/ArquiteturaSegurancaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.embrapa.arquitetura.seguranca;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArquiteturaSegurancaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
